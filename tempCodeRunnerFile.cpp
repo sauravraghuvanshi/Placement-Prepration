9d8fecafa6b8a6bcc127e4de10df2eeb33ee6880
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2021-01-01 11:59:25 +0530

    added 3.1 code

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,18 @@
-#include<iostream>  
-  
-using namespace std; 
-  
-// main function - 
-// where the execution of program begins 
-int main() 
-{ 
-    // prints hello world 
-    cout<<"Hello World"; 
-      
-    return 0; 
-} 
+// C++ program to sizes of data types
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "Size of char : " << sizeof(char) << " byte" << endl;
+    cout << "Size of int : " << sizeof(int) << " bytes" << endl;
+    cout << "Size of short int : " << sizeof(short int) << " bytes" << endl;
+    cout << "Size of long int : " << sizeof(long int) << " bytes" << endl;
+    cout << "Size of signed long int : " << sizeof(signed long int) << " bytes" << endl;
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int) << " bytes" << endl;
+    cout << "Size of float : " << sizeof(float) << " bytes" <<endl;
+    cout << "Size of double : " << sizeof(double) << " bytes" << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << " bytes" <<endl;
+     
+    return 0;
+}
