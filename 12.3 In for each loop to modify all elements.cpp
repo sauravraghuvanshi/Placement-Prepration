2c8f236112a89843d0bdd1686f5c5c270bc76294
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2021-02-18 22:22:34 +0530

    added 12.3 In for each loop to modify all elements.cpp code

---
+++ 12.3 In for each loop to modify all elements.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() 
+{ 
+	vector<int> vect{ 10, 20, 30, 40 }; 
+
+	// We can modify elements if we 
+	// use reference
+	for (int &x : vect) 
+		x = x + 5;
+
+	// Printing elements
+	for (int x : vect) 
+	cout << x << " "; 
+
+	return 0; 
+} 
