5338c7af86b2065c6edc227bc4e436d0077dde65
sauravraghuvanshi <sauravraghuvanshi24@gmail.com>
2021-08-21 19:00:36 +0530

    added insertio nsort code

---
+++ Module 2 - Searching Sorting and Analysis/InsertionSort.py
@@ -0,0 +1,16 @@
+def InsertionSort(arr):
+    for i in range(2, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+#arr = list(input("Enter the element of arr: ").split()) ## if you have to take array dynamically
+arr = [1, 8, 15, 2, 10]
+InsertionSort(arr)
+
+for v in arr:
+    print(v)
